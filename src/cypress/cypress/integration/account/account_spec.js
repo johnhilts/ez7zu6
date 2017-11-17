0784c9e1429999e520804e2bba927f9a962fc210
John Hilts <johnhilts@live.com>
2017-11-16 21:19:10 -0800

    Refactored tests for clarity and DRYness
    decided to go with simple functions, instead of workflows and POMs ...
    I'm thinking it might be faster to use small helper functions instead of
    building up a bunch of POMs and workflows ... still "imperative shell / functional core", and still very legible.
    We'll see how maintenance holds up ...
    The idea is to play to JS strengths ...

--- src/cypress/cypress/integration/account/account_spec.js
+++ src/cypress/cypress/integration/account/account_spec.js
@@ -1,39 +1,33 @@
+import * as helper from '../Helper/AccountHelper';
+
 let baseUrl = 'http://localhost:17726';
 describe('account', () => {
     context('authentication', () => {
         it('should prevent unauthenticated access of profile', () => {
-            let expected = '/account/login';
-            cy.visit(`${baseUrl}/profile`).url();
+            cy.visit(`${baseUrl}${helper.authenticationRequiredPath}`).url();
             cy.location().should((location) => {
+                let expected = helper.loginPath;
                 let actual = location.pathname.toLowerCase();
                 expect(actual).to.eq(expected)
             })
         })
         it('should see profile upon login', () => {
-            let expected = `${baseUrl}/profile`;
+            let expected = `${baseUrl}${helper.authenticationRequiredPath}`;
             cy.visit(expected);
-            let username = 'john@test.com';
-            let password = 'test123';
-            cy.get('#txtUserName').type(username).should('have.value', username);
-            cy.get('#txtPassword').type(password).should('have.value', password);
-            cy.get('button').click();
+            helper.login();
             cy.location().should((location) => {
                 let actual = location.href.toLowerCase(); 
                 expect(actual).to.eq(expected)
             })
         })
         it('cannot access profile after logout', () => {
-            cy.visit(`${baseUrl}/account/login`);
-            let username = 'john@test.com';
-            let password = 'test123';
-            cy.get('#txtUserName').type(username).should('have.value', username);
-            cy.get('#txtPassword').type(password).should('have.value', password);
-            cy.get('button').click();
+            cy.visit(`${baseUrl}${helper.loginPath}`);
+            helper.login();
             cy.contains('a', 'Logout').click()
-            let unexpected = `${baseUrl}/profile`;
-            cy.visit(unexpected);
+            let unaccessable = `${baseUrl}${helper.authenticationRequiredPath}`;
+            cy.visit(unaccessable);
             cy.location().should((location) => {
-                let expected = '/account/login';
+                let expected = helper.loginPath;
                 let actual = location.pathname.toLowerCase(); 
                 expect(actual).to.eq(expected)
             })

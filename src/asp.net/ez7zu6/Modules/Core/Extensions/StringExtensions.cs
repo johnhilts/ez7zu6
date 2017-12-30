e21b87d33546f74e61660d1b58f5ab64f112bbf3
John Hilts <johnhilts@live.com>
2017-12-30 06:19:59 -0800

    remove uneeded code

--- src/asp.net/ez7zu6/Modules/Core/Extensions/StringExtensions.cs
+++ src/asp.net/ez7zu6/Modules/Core/Extensions/StringExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 
 namespace ez7zu6.Core.Extensions
 {
@@ -8,26 +7,6 @@ namespace ez7zu6.Core.Extensions
         public static byte[] GetBytes(this string str)
         {
             return Encoding.UTF8.GetBytes(str);
-        }
-
-        // TODO: do we need this?? we'll know for sure once we implement the pwd check ...
-        public static byte[] GetBytesFromHexString(this string hexString)
-        {
-            if (string.IsNullOrEmpty(hexString))
-                return null;
-
-            var bytes = new byte[hexString.Length / 2];
-            int strIndex;
-            int byteIndex = 0;
-
-            for (strIndex = 0; strIndex < hexString.Length; strIndex += 2)
-            {
-                string hexNumber = hexString.Substring(strIndex, 2);
-                bytes[byteIndex] = Convert.ToByte(hexNumber, 16);
-                byteIndex++;
-            }
-
-            return bytes;
         }
     }
 }
